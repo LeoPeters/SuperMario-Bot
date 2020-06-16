8ffa099a713a45aae3ba783cd73527238c6ae8a1
MichiBab <Michael.Babic@haw-hamburg.de>
2020-06-16 22:23:45 +0200

    removed cnn

--- SuperMarioAi/main.cpp
+++ SuperMarioAi/main.cpp
@@ -15,57 +15,19 @@
 #define DEBUG true
 FILE* fDummy;
 void CreateConsole();
-
-
-static std::vector<unsigned char> getEnvDataDummy() {
-    std::vector<unsigned char> v(4 * 15 * 15);
-    std::generate(v.begin(), v.end(), std::rand);
-    return v;
-}
+static void DQNexample();
+static std::vector<unsigned char> getEnvDataDummy();
 
 int main(int argc, char* argv[])
 {
     if (DEBUG) {
         CreateConsole();
     }
-    DQNAgent agent( /*gamma*/0.999, /*epsilon*/0.1, /*learnrate*/0.001, 
-                    /*eps_min*/0.001, /*eps_dec*/0.0001, /*mem_size*/10000, 
-                    /*batch_size*/16, /*update_target*/10, /*actions*/12, 
-                    /*input_shape: DIM, HEIGHT, WIDTH*/4, 15, 15 );
-
-    auto start = std::chrono::high_resolution_clock::now();
-    for (int i = 0; i < 80; i++) {
-    srand(time(NULL));
-    int random = rand();
-
-    //ABLAUF:
-    //GET INPUT 4 15 15 => v
-    auto state = getEnvDataDummy();
-    //act
-    int action = agent.choose_action(state);
-    std::cout <<"action: "<< action << std::endl;
-    //GET NEW ENV AFTER ACTION
-    //DO ( ACTION ) 
-    auto nextstate = getEnvDataDummy();
-    // REWARD = GET REWARD()
-    float reward = (float)random / (RAND_MAX); /*DUMMY*/
-    // AM I DONE = GET DONE?()
-    bool done = false; /*DUMMY*/
-    //jetzt: store transition
-    agent.store_transition(/*state*/state, /*action*/action, /*reward*/reward, /*newstate*/nextstate, /*done*/done);
-    //std::cout << "in learn\n";
-    agent.learn(); //hier wird er nicht lernen, da memory size < batchsize ( 1 < 2)
-    //std::cout << i<<" ";
+    bool DQNskipexample = true;
+    if (!DQNskipexample) {
+        DQNexample();
     }
-    auto stop = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
-    std::cout << duration.count() << "ms for inputting 80* 4*15*15 pictures and learning after the 8th" << std::endl;
-
-
-    char x;
-    std::cin >> x;
-    return 0;
-
+   
     AiController controller(argc, argv);
     std::thread controllerThread(&AiController::run, &controller);
     controller.runGui();
@@ -98,3 +60,51 @@ void CreateConsole()
     std::wcerr.clear();
     std::wcin.clear();
 }
+
+
+
+static std::vector<unsigned char> getEnvDataDummy() {
+    std::vector<unsigned char> v(4 * 15 * 15);
+    std::generate(v.begin(), v.end(), std::rand);
+    return v;
+}
+
+static void DQNexample() {
+    DQNAgent agent( /*gamma*/0.999, /*epsilon*/0.1, /*learnrate*/0.001,
+        /*eps_min*/0.001, /*eps_dec*/0.0001, /*mem_size*/10000,
+        /*batch_size*/16, /*update_target*/10, /*actions*/12,
+        /*input_shape: DIM, HEIGHT, WIDTH*/4, 15, 15);
+
+    auto start = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < 80; i++) {
+        srand(time(NULL));
+        int random = rand();
+
+        //ABLAUF:
+        //GET INPUT 4 15 15 => v
+        auto state = getEnvDataDummy();
+        //act
+        int action = agent.choose_action(state);
+        std::cout << "action: " << action << std::endl;
+        //GET NEW ENV AFTER ACTION
+        //DO ( ACTION ) 
+        auto nextstate = getEnvDataDummy();
+        // REWARD = GET REWARD()
+        float reward = (float)random / (RAND_MAX); /*DUMMY*/
+        // AM I DONE = GET DONE?()
+        bool done = false; /*DUMMY*/
+        //jetzt: store transition
+        agent.store_transition(/*state*/state, /*action*/action, /*reward*/reward, /*newstate*/nextstate, /*done*/done);
+        //std::cout << "in learn\n";
+        agent.learn(); //hier wird er nicht lernen, da memory size < batchsize ( 1 < 2)
+        //std::cout << i<<" ";
+    }
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+    std::cout << duration.count() << "ms for inputting 80* 4*15*15 pictures and learning after the 8th" << std::endl;
+
+
+    char x;
+    std::cin >> x;
+}
+
